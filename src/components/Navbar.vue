f013336f549e296a5c4b1fd72c0c6008344fedb3
philipp <50250716+Kuuhhl@users.noreply.github.com>
2024-05-03 20:17:20 +0100

    improved hover fx

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,7 +10,6 @@
 						alt="Logo" />
 					<span class="hidden md:inline">Drone Directory</span>
 				</RouterLink>
-
 				<button alt="Toggle Navbar" @click="toggleMenu" class="text-3xl md:hidden">
 					<FontAwesomeIcon :icon="['fas', isMenuOpen ? 'fa-times' : 'fa-bars']" />
 				</button>
@@ -19,15 +18,16 @@
 			<!-- Menu Items -->
 			<div :class="{ 'flex-col md:flex-row': isMenuOpen, 'hidden': !isMenuOpen }"
 				class="md:flex items-center gap-5 w-full md:w-auto md:justify-between">
-				<div class="flex flex-col md:flex-row gap-3 md:gap-5 mt-3 md:mt-0 hover:underline">
-					<RouterLink v-for="link in linksLeft" :key="link.text" :to="link.link" @click="isMenuOpen = false">
+				<div class="flex flex-col md:flex-row gap-3 md:gap-5 mt-3 md:mt-0">
+					<RouterLink v-for="link in linksLeft" :key="link.text" :to="link.link" @click="isMenuOpen = false"
+						class="relative group py-2 after:absolute after:bottom-0 after:left-0 after:bg-black after:h-0.5 after:w-0 hover:after:w-full after:transition-all after:duration-300 md:after:left-1/2 md:after:-translate-x-1/2">
 						{{ link.text }}
 					</RouterLink>
 				</div>
 				<div class="flex flex-col md:flex-row md:items-center gap-3 md:gap-5 mt-3 md:mt-0">
 					<component v-for="item in props.linksRight" :key="item.text" :is="item.link ? 'RouterLink' : 'button'"
 						:to="item.link || null" @click="item.link ? executeAction(item) : executeAction(item)"
-						class="cursor-pointer text-start hover:underline">
+						class="relative group py-2 after:absolute after:bottom-0 after:left-0 after:bg-black after:h-0.5 after:w-0 hover:after:w-full after:transition-all after:duration-300 md:after:left-1/2 md:after:-translate-x-1/2 cursor-pointer text-start">
 						{{ item.text }}
 					</component>
 					<component :is="props.button.link ? 'RouterLink' : 'button'" :to="props.button.link || null"
@@ -47,7 +47,6 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { RouterLink } from 'vue-router'
-
 
 library.add(faBars, faTimes)
 
@@ -71,6 +70,7 @@ const isMenuOpen = ref(false)
 function toggleMenu() {
 	isMenuOpen.value = !isMenuOpen.value
 }
+
 const executeAction = (item) => {
 	isMenuOpen.value = false;
 	if (item.function) {
